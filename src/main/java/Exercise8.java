48cacaa56f82bf38b7268dd95a508053b1a77da1
umranrazack02 <umranrazack02@outlook.com>
2021-08-01 23:55:53 -0400

    challenge

--- src/main/java/Exercise8.java
+++ src/main/java/Exercise8.java
@@ -11,16 +11,35 @@ public class Exercise8 {
         Scanner in = new Scanner(System.in);
 
         System.out.print("How many people? ");
+        int people = 0;
         String peo = in.nextLine();
-        int people = Integer.parseInt(peo);
+        try {
+            people = Integer.parseInt(peo);
+        } catch(NumberFormatException e) {
+            System.out.println("Please enter a numeric value!");
+            System.exit(0);
+        }
+
 
         System.out.print("How many pizzas do you have? ");
         String piz = in.nextLine();
-        int pizza = Integer.parseInt(piz);
+        int pizza = 0;
+        try {
+            pizza = Integer.parseInt(piz);
+        } catch(NumberFormatException e) {
+            System.out.println("Please enter a numeric value!");
+            System.exit(0);
+        }
 
         System.out.print("How many slices per pizza? ");
         String sli = in.nextLine();
-        int slices = Integer.parseInt(sli);
+        int slices = 0;
+        try {
+            slices = Integer.parseInt(sli);
+        } catch(NumberFormatException e) {
+            System.out.println("Please enter a numeric value!");
+            System.exit(0);
+        }
 
         System.out.printf("%d people with %d pizzas (%d slices)\n", people, pizza, slices);
         System.out.printf("Each person gets %d slices of pizza.\n", (slices * pizza) / people);
